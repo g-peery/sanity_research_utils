8c5b34701925c7666efbc4b775c0818d022d1178
Gabriel Peery <peerygabriel@gmail.com>
2025-11-05 16:41:30 -0600

    Define custom style with dictionary instead of file

--- sanity_utils/utils.py
+++ sanity_utils/utils.py
@@ -6,12 +6,29 @@ import os
 Stats = namedtuple("Stats", ["min", "max", "mean", "var", "count"])
 
 
+_style_dict = {
+    "axes.labelsize" : 16,
+    "xtick.labelsize" : 16,
+    "ytick.labelsize" : 16,
+    "legend.fontsize" : 16,
+    "legend.frameon" : True,
+    "legend.framealpha" : 0.8,
+    "figure.titlesize" : 24,
+    "figure.titleweight" : "bold",
+    "legend.loc" : "upper right",
+    "savefig.dpi" : "figure",
+    "figure.dpi" : 800,
+    "savefig.bbox" : "tight",
+    "savefig.transparent" : False
+}
+
+
 def custom_plt_style():
     """
     Configures matplotlib.pyplot to use a specific style.
     """
     plt.style.use([
         "ggplot",
-        os.path.join(os.path.dirname(__file__), os.pardir, "figures.mplstyle")
+        _style_dict
     ])
 
